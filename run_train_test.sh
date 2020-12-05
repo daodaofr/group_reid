da04214fc28247007103e8d52c85b189742ae4e6
daodaofr <yanyichao91@gmail.com>
2020-12-05 21:36:56 +0400

    Update run_train_test.sh

--- run_train_test.sh
+++ run_train_test.sh
@@ -1,1 +1,1 @@
-nohup python -u main_group_gcn_siamese_relative_part_1.py --max-epoch 300 --stepsize 100 --eval-step 300 --gpu-devices 0 > log.txt 2>&1 &
+nohup python -u main_group_gcn_siamese_relative_part_1.py --data-root /raid/yy1/data/cuhk/Image/SSM --max-epoch 300 --stepsize 100 --eval-step 300 --gpu-devices 0 > log.txt 2>&1 &
